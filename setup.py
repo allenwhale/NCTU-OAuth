1f31c62f39fa518a0c1219fc2e83a55b95a04a80
allenwhale <allenwhale@yahoo.com.tw>
2016-09-04 14:20:02 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import sys, os
+
+VERSION = '0.1.0'
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+	name='NCTU-Oauth',
+	version=VERSION,
+	description="adds NCTU-Oauth support to tornado",
+	long_description=long_description,
+	classifiers=[],
+	keywords='python tornado nctu oauth nctu-oauth',
+	author='allenwhale',
+	author_email='allencat850502@gmail.com',
+	url='https://github.com/allenwhale/NCTU-OAuth',
+	license='BSD',
+	packages=['nctu_oauth'],
+	include_package_data=True,
+	zip_safe=True,
+	install_requires=['tornado'])
+
